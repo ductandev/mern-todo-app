012af221e20ce2980933b8dbe22f805922699012
Tan.nd <nguyenductan998@gmail.com>
2026-05-27 00:38:42 +0700

    update: axios

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios"
+
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:9090/api"
+
 const instance = axios.create({
-  baseURL: "http://localhost:9090/api"
+  baseURL,
 })
+
 export default instance
